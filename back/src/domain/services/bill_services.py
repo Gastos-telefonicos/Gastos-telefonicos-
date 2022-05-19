8486d9e29d4dd07b1492cb17a92b839e377c6271
Claudio <Claucode97@gmail.com>
2022-05-19 10:08:40 +0200

    Se crea método para retornar lista de objetos

--- back/src/domain/services/bill_services.py
+++ back/src/domain/services/bill_services.py
@@ -8,24 +8,18 @@ class Pdf_Invoice:
         self.pdf_path = pdf_path
 
     def convert_base64_to_pdf(self, base64_string):
-        with open(self.pdf_path, "wb") as theFile:
-            theFile.write(base64.b64decode(base64_string))
+        with open(self.pdf_path, "wb") as the_file:
+            the_file.write(base64.b64decode(base64_string))
         pfd_num_and_cost = self.get_text_from_all_pdf_pages()
-        return pfd_num_and_cost
-    
+        object_list = self.convert_tuple_list_to_object_list(pfd_num_and_cost)
+        return object_list
+
     def get_mobile_numbers_and_their_cost(self, text):
         pattern = r"([6|7]\d{2} \d{3} \d{3})\n(-?\d{1,4},\d{1,4}\n)*(\d{1,4},\d{1,4}) •"
         match = re.findall(pattern, text)
-
-        print(match)
-        if match:
-            print("Yes, there is at least one match!")
-        else:
-            print("No match")
         return match
 
     def get_text_from_all_pdf_pages(self):
-
         pdf_file = open(self.pdf_path, "rb")
         pdf_reader = PyPDF2.PdfFileReader(pdf_file)
         num_pages = pdf_reader.getNumPages()
@@ -37,3 +31,9 @@ class Pdf_Invoice:
         list_of_phones_and_cost = self.get_mobile_numbers_and_their_cost(whole_text)
         pdf_file.close()
         return list_of_phones_and_cost
+
+    def convert_tuple_list_to_object_list(self, tuple_list):
+        object_list = []
+        for each_tuple in tuple_list:
+            object_list.append({"phone": each_tuple[0], "cost": each_tuple[2]})
+        return object_list
