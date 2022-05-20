00d7caa7b77feb4557680df21daf9015b1e638e5
ainhoa <mendibeainhoa@gmail.com>
2022-05-20 10:08:56 +0200

    -

--- back/test/info/test_get_phones.py
+++ back/test/info/test_get_phones.py
@@ -8,8 +8,12 @@ def test_should_return_phones_proyects_and_costs():
     app = create_app(repositories={"phones": phone_repository})
     client = app.test_client()
 
-    phone_one = Phone(id="bill-1", phone="1644541544", costs=35, proyect="GEN1234")
-    phone_two = Phone(id="bill-2", phone="1644541545", costs=36, proyect="GEN5678")
+    phone_one = Phone(
+        phone="1644541544", costs=35, proyect="GEN1234", description="iñaki"
+    )
+    phone_two = Phone(
+        phone="1644541545", costs=36, proyect="GEN5678", description="laura"
+    )
     phone_repository.save(phone_one)
     phone_repository.save(phone_two)
 
@@ -18,6 +22,16 @@ def test_should_return_phones_proyects_and_costs():
 
     # ASSERT (then)
     assert response.json == [
-        {"id": "bill-1", "phone": "1644541544", "costs": 35, "proyect": "GEN1234"},
-        {"id": "bill-2", "phone": "1644541545", "costs": 36, "proyect": "GEN5678"},
+        {
+            "phone": "1644541544",
+            "costs": 35,
+            "proyect": "GEN1234",
+            "description": "iñaki",
+        },
+        {
+            "phone": "1644541545",
+            "costs": 36,
+            "proyect": "GEN5678",
+            "description": "laura",
+        },
     ]
