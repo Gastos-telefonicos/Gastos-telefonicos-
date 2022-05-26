b7343734e500fd74d10673f37eed93ac8ed53e80
ainhoa <mendibeainhoa@gmail.com>
2022-05-26 10:18:58 +0200

    modify PUT endpoint

--- back/test/info/test_update_projets.py
+++ back/test/info/test_update_projets.py
@@ -6,26 +6,27 @@ from src.domain.phones import PhonesRepository, Phone
 
 
 def test_should_update_projets():
-    phones_repository = PhonesRepository(temp_file())
-    app = create_app(repositories={"phones": phones_repository})
+    phone_repository = PhonesRepository(temp_file())
+    app = create_app(repositories={"phones": phone_repository})
     client = app.test_client()
 
-    phone_one = Phone(phone="1644541544", project="GEN1234", description="JOSEBA")
-    phones_repository.save(phone_one)
+    phone_one = Phone(phone="164454154", project="GEN1234", description="JOSEBA")
+    phone_repository.save(phone_one)
 
     body = {
-        "phone": "1644541544",
+        "phone": "164454154",
         "project": "GEN4567",
         "description": "JOSEBA",
     }
-    response = client.put("/api/phones/1644541544", json=body)
+
+    response = client.put("/api/phones/164454154", json=body)
 
     assert response.status_code == 200
     response_get = client.get("/api/phones")
 
     assert response_get.json == [
         {
-            "phone": "1644541544",
+            "phone": "164454154",
             "project": "GEN4567",
             "description": "JOSEBA",
         }
